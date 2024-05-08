973f10c823fa9a2ff46f6ce35a285d0213f75475
Braulio_Bolano <brauliobolanostrujillo@gmail.com>
2024-05-07 20:26:11 -0400

    Added changes to PostFromt

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import Image from "next/image";
 import { Button } from "@/components/ui/button";
+import UserInformation from "@/components/UserInformation";
+import PostForm from "@/components/PostForm";
 
 export default function Home() {
   return (
-    <div className="flex">
-      <section>{/* UserInformation */}</section>
+    <div className="grid grid-cols-8 mt-5 sm:px-5">
+      <section className="hidden md:inline md:col-span-2">
+        <UserInformation />
+      </section>
 
-      <section>
-        {/* PostForm */}
+      <section className="col-span-full md:col-span-6 xl:col-span-4 xl:max-w-xl mx-auto w-full">
+        <PostForm />
         {/* PostFeed */}
       </section>
 
-      <section>{/* Widget */}</section>
+      <section className="hidden xl:inline justify-center col-span-2">
+        {/* Widget */}
+      </section>
     </div>
   );
 }
---
+++ components/PostForm.tsx
@@ -0,0 +1,102 @@
+"use client";
+import { useUser } from "@clerk/nextjs";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Button } from "./ui/button";
+import { ImageIcon, X, XIcon } from "lucide-react";
+import { useRef, useState } from "react";
+
+function PostForm() {
+  const ref = useRef<HTMLFormElement>(null);
+  const fileInputRef = useRef<HTMLInputElement>(null);
+  const { user } = useUser();
+  const [preview, setPreview] = useState<string | null>(null);
+
+  const handleImageChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setPreview(URL.createObjectURL(file));
+    }
+  };
+
+  return (
+    <div className="mb-2">
+      <form
+        ref={ref}
+        action={(FormData) => {
+          console.log(FormData);
+        }}
+        className="p-3 bg-white rounded-lg border"
+      >
+        <div className="flex items-center space-x-2">
+          {/* //TODO Make avatar a separate component */}
+          <Avatar>
+            {user?.id ? (
+              <AvatarImage src={user?.imageUrl} />
+            ) : (
+              <AvatarImage src="https://github.com/shadcn.png" />
+            )}
+
+            <AvatarFallback>
+              {user?.firstName?.charAt(0)}
+              {user?.lastName?.charAt(0)}
+            </AvatarFallback>
+          </Avatar>
+
+          <input
+            type="text"
+            name="postInput"
+            placeholder="Start writing a post..."
+            className="flex-1 outline-none rounded-full px-4 py-3 border"
+          />
+          <input
+            ref={fileInputRef}
+            type="file"
+            name="image"
+            accept="image/*"
+            hidden
+            onChange={handleImageChange}
+          />
+          <button type="submit" hidden>
+            Post
+          </button>
+        </div>
+
+        {/* Preview conditional check */}
+
+        {preview && (
+          <div className="mt-3">
+            <img src={preview} alt="Preview" className="w-full object-cover" />
+          </div>
+        )}
+
+        <div className="flex justify-end mt-2 space-x-2">
+          <Button type="button" onClick={() => fileInputRef.current?.click()}>
+            <ImageIcon className="mr-2" size={16} color="currentColor" />
+            {preview ? "Change" : "Add"} Image
+          </Button>
+
+          {/* Add a remove preview button  */}
+
+          {preview && (
+            <Button
+              type="button"
+              variant="outline"
+              onClick={() => {
+                setPreview(null);
+                fileInputRef.current!.value = "";
+              }}
+            >
+              <XIcon className="mr-2" size={16} color="currentColor" />
+              Remove image
+            </Button>
+          )}
+
+          {/* Add a post button */}
+        </div>
+      </form>
+      <hr className="mt-2 border-gray-300" />
+    </div>
+  );
+}
+
+export default PostForm;
